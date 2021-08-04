28fdc2dad0bd791e9c4707acf24b1ef83f253953
Kreig Zimmerman <myself@kreig.me>
2021-08-04 14:23:45 -0400

    style: prettier via husky first pass

--- components/layout.js
+++ components/layout.js
@@ -15,44 +15,44 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content={description} />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content={description} />
         <meta
-          property="og:image"
+          property='og:image'
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <header className={styles.header}>
         {home ? (
           <>
             <Image
-              src="/images/profile.jpg"
+              src='/images/profile.jpg'
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
               alt={name}
-              width="535"
-              height="539"
+              width='535'
+              height='539'
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
-            <Link href="/">
+            <Link href='/'>
               <a>
                 <Image
-                  src="/images/profile.jpg"
+                  src='/images/profile.jpg'
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
-                  width="535"
-                  height="539"
+                  width='535'
+                  height='539'
                 />
               </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href="/">
+              <Link href='/'>
                 <a className={utilStyles.colorInherit}>{name}</a>
               </Link>
             </h2>
@@ -62,7 +62,7 @@ export default function Layout({ children, home }) {
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href="/">
+          <Link href='/'>
             <a>← Back to home</a>
           </Link>
         </div>
